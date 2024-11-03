c81cd7478354fa051873435c3c313ad82a011b2b
Teddyburgonde <bandama.teddy@gmail.com>
2024-11-04 00:09:10 +0100

    to do fixed norme for ft_strjoin, maybe delete s1=NULL

--- get_next_line.c
+++ get_next_line.c
@@ -6,149 +6,120 @@
 /*   By: teddybandama <teddybandama@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:22:37 by teddybandam       #+#    #+#             */
-/*   Updated: 2024/11/03 22:40:51 by teddybandam      ###   ########.fr       */
+/*   Updated: 2024/11/04 00:06:37 by teddybandam      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
 
-#include <stdlib.h>
-#include <unistd.h>
-
-static char *read_and_concatenate(int fd, char *stash) 
+static char	*read_and_concatenate(int fd, char *stash)
 {
-    char *buff;
-    int byte_read;
+	char	*buff;
+	int		byte_read;
 
-    byte_read = 1;
-    buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buff) 
-    {
-        free(stash);
-        return NULL;
-    }
-    byte_read = read(fd, buff, BUFFER_SIZE);
-    if (byte_read < 0) 
-    {
-        free(buff);
-        free(stash);
-        return NULL; 
-    }
-    buff[byte_read] = '\0'; 
-    stash = ft_strjoin(stash, buff);
-    if (!stash) 
-    {
-        free(buff);
-        return NULL;
-    }
-    free(buff); 
-    return stash;
+	byte_read = 1;
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+	{
+		free(stash);
+		return (NULL);
+	}
+	byte_read = read(fd, buff, BUFFER_SIZE);
+	if (byte_read < 0)
+	{
+		free(buff);
+		free(stash);
+		return (NULL);
+	}
+	buff[byte_read] = '\0';
+	stash = ft_strjoin(stash, buff);
+	if (!stash)
+	{
+		free(buff);
+		return (NULL);
+	}
+	free(buff);
+	return (stash);
 }
 
-static char *read_line(char *stash, int fd)
+static char	*read_line(char *stash, int fd)
 {
-    if (stash == NULL)
-    {
-        stash = malloc(1);
-        if (!stash)
-            return (NULL);
-        stash[0] = '\0';
-    }
-    while (ft_strchr(stash, '\n') == NULL)
-    {
-        stash = read_and_concatenate(fd, stash);
-        if (!stash) 
-            return NULL;
-        if (stash[0] == '\0') 
-            break;
-    }
-    return stash;
+	if (stash == NULL)
+	{
+		stash = malloc(1);
+		if (!stash)
+			return (NULL);
+		stash[0] = '\0';
+	}
+	while (ft_strchr(stash, '\n') == NULL)
+	{
+		stash = read_and_concatenate(fd, stash);
+		if (!stash)
+			return (NULL);
+		if (stash[0] == '\0')
+			break ;
+	}
+	return (stash);
 }
 
-static char *extract_line(char *stash)
-{
-    char *line;
-    int i;
-    int line_length;
-
-    i = 0;
-
-    if (!stash[i])
-        return (NULL);
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    line_length = i;
-    if (stash[i] == '\n')
-        line_length++;
-    line = ft_substr(stash, 0, line_length);
-    return (line); 
-}
-
-static char *extract_surplus_line(char *stash)
-{
-    char    *surplus_line;
-    int i;
-    int j;
-
-    j = 0;
-    if (!stash)
-        return (NULL);
-    i = 0;
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    if (stash[i] != '\n')
-        return (NULL);
-    surplus_line = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
-    if (!surplus_line)
-        return (NULL);
-    i++;
-    while (stash[i])
-    {
-        surplus_line[j] = stash[i]; 
-        i++;
-        j++;
-    }
-    surplus_line[j] = '\0';
-    free(stash);
-    return (surplus_line);
-}
-
-char *get_next_line(int fd)
-{
-    static char *stash = NULL;
-    char *line;
-    
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    stash = read_line(stash, fd);
-    if (!stash)
-        return NULL;
-    line = extract_line(stash);
-    stash = extract_surplus_line(stash);
-    return (line);
-}
-
-#include <stdio.h>
-
-int	main(void)
+static char	*extract_line(char *stash)
 {
 	char	*line;
 	int		i;
-	int		fd1;
-	fd1 = open("test.txt", O_RDONLY);
-    if (fd1 < 0)
-        return (1);
-	i = 1;
-	while (1)
+	int		line_length;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line_length = i;
+	if (stash[i] == '\n')
+		line_length++;
+	line = ft_substr(stash, 0, line_length);
+	return (line);
+}
+
+static char	*extract_surplus_line(char *stash)
+{
+	char	*surplus_line;
+	int		i;
+	int		j;
+
+	j = 0;
+	if (!stash)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] != '\n')
+		return (NULL);
+	surplus_line = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!surplus_line)
+		return (NULL);
+	i++;
+	while (stash[i])
 	{
-        line = get_next_line(fd1);
-        if (line == NULL)
-            break;
-        printf("line [%02d]: %s", i, line);
-        free(line);
-	    i++;
+		surplus_line[j] = stash[i];
+		i++;
+		j++;
 	}
-	close(fd1);
-	return (0);
+	surplus_line[j] = '\0';
+	free(stash);
+	return (surplus_line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash = NULL;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_line(stash, fd);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = extract_surplus_line(stash);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,20 @@
 /*   By: teddybandama <teddybandama@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:28:29 by teddybandam       #+#    #+#             */
-/*   Updated: 2024/11/03 21:03:52 by teddybandam      ###   ########.fr       */
+/*   Updated: 2024/11/03 23:42:40 by teddybandam      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
- #define GET_NEXT_LINE_H
-
+# define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
-#define BUFFER_SIZE 5
- 
-char *get_next_line(int fd);
-int	ft_strlen(const char *str);
+# define BUFFER_SIZE 5
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, int len);
-
 
 #endif 
